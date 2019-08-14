369aa597a3c29e8b99f0d874e76bbad876eb5923
Ben Hanna <benpaulhanna@gmail.com>
2019-08-13 20:24:05 -0400

    Added styling to login form.

--- package.lisp
+++ package.lisp
@@ -29,7 +29,10 @@
 (defmacro with-layout (&rest body)
   `(cl-who:with-html-output-to-string (output)
      (:html
-      (:body ,@body))))
+      (:head
+       (:link :rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css"))
+      (:body :style "padding-top: 32px; background-color: #eee;"
+       (:div :class "columns" ,@body)))))
 
 (defun start-server ()
   (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242))
@@ -58,15 +61,24 @@
                (hunchentoot:start-session))
              (setf message "Login failed."))))
       (with-layout
-          (:h4 "Login")
-          (:div 
-             (:form :method "post"
-                (:input :type "email" :name "email")
-                (:input :type "password" :name "password")
-                (:button :type "submit" "Login")))
-        (:p (cl-who:str message))
-        (when hunchentoot:*session*
-            (cl-who:htm (:p (cl-who:str "Session Active"))))))))
+          (:div :class "column")
+          (:div :class "column is-narrow"
+                (:div :class "card" :style "width: 400px;"
+                      (:div :class "card-content"
+                            (:h2 :class "subtitle is-4 has-text-centered" "Log in to your account")
+                            (when message
+                              (cl-who:htm (:article :class "message is-danger has-text-centered"
+                                                    (:div :class "message-body" :style "padding: 0.5em;" (cl-who:str message)))))
+                            (:form :method "post"
+                                   (:div :class "field"
+                                         (:p :class "control"
+                                             (:input :class "input" :placeholder "Email" :type "email" :name "email")))
+                                   (:div :class "field"
+                                         (:p :class "control"
+                                             (:input :class "input" :placeholder "Password" :type "password" :name "password")))
+                                   (:button :class "button is-link" :style "width: 100%;" :type "submit" "Login")))))
+        (:div :class "column")
+        ))))
 
 (defun logout-page ()
   (hunchentoot:define-easy-handler (logout :uri "/logout") ()
